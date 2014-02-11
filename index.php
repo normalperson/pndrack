9faf24ee57db1cf066ead2e84d85ec0edfead1fc
esp <esp@phiorion.com>
2014-02-11 10:57:53 +0800

    change index to landing page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'init.inc.php');
-redirect('/'.APP.'/web/Dashboard/plateSumm');
+redirect('/'.APP.'/web/Home/Home');
 
 ?>
--- info/esp_sql.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE pflex.smshelfsetting
- ADD sf_freeslot VARCHAR(20) DEFAULT 'Y' AFTER sf_orgid;
